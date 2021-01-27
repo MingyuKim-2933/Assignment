bf8141d56d539d9cd194c806a6492b1973a02ba7
MingyuKim-2933 <68273065+MingyuKim-2933@users.noreply.github.com>
2021-01-27 18:04:32 +0900

    Update Tic-Tac-Toe.py

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -5,7 +5,7 @@ import math
 from tqdm import tqdm
 
 
-class Environment():  # 3x3 환경
+class Environment33to3():  # 3x3 환경 승리조건 3개
 
     def __init__(self):
         # 보드는 0으로 초기화된 9개의 배열로 준비
@@ -79,85 +79,240 @@ class Environment():  # 3x3 환경
             print("|")
             print("+----+----+----+")
 
+class Environment44to4():  # 4x4환경 승리조건 4개
 
-# class Environment():  # 5x5환경
-#
-#     def __init__(self):
-#         # 보드는 0으로 초기화된 25개의 배열로 준비
-#         # 게임종료 : done = True
-#         self.board_a = np.zeros(25)
-#         self.done = False
-#         self.reward = 0
-#         self.winner = 0
-#         self.print = False
-#
-#
-#     def move(self, p1, p2, player):
-#         # 각 플레이어가 선택한 행동을 표시 하고 게임 상태(진행 또는 종료)를 판단
-#         # p1 = 1, p2 = -1로 정의
-#         # 각 플레이어는 행동을 선택하는 select_action 메서드를 가짐
-#         if player == 1:
-#             pos = p1.select_action(env, player)
-#         else:
-#             pos = p2.select_action(env, player)
-#
-#         # 보드에 플레이어의 선택을 표시
-#         self.board_a[pos] = player
-#         if self.print:
-#             print(player)
-#             self.print_board()
-#         # 게임이 종료상태인지 아닌지를 판단
-#         self.end_check(player)
-#
-#         return self.reward, self.done
-#
-#     # 현재 보드 상태에서 가능한 행동(둘 수 있는 장소)을 탐색하고 리스트로 반환
-#     def get_action(self):
-#         observation = []
-#         for i in range(25):
-#             if self.board_a[i] == 0:
-#                 observation.append(i)
-#         return observation
-#
-#     # 게임이 종료(승패 또는 비김)됐는지 판단
-#     def end_check(self, player):
-#         # 0 1 2
-#         # 3 4 5
-#         # 6 7 8
-#         # 승패 조건은 가로, 세로, 대각선 이 -1 이나 1 로 동일할 때
-#         end_condition = ((0, 1, 2), (1, 2, 3), (2, 3, 4), (5, 6, 7), (6, 7, 8), (7, 8, 9), (10, 11, 12), (11, 12, 13), (12, 13, 14), (15, 16, 17), (16, 17, 18), (17, 18, 19), (20, 21, 22), (21, 22, 23), (22, 23, 24),
-#                          (0, 6, 12), (6, 12, 18), (12, 18, 24), (5, 11, 17), (11, 17, 23), (10, 16, 22), (1, 7, 13), (7, 13, 19), (2, 8, 14),
-#                          (4, 8, 12), (8, 12, 16), (12, 16, 20), (3, 7, 11), (7, 11, 15), (2, 6, 10), (9, 13, 17), (13, 17, 21), (14, 18, 22),
-#                          (0, 5, 10), (5, 10, 15), (10, 15, 20), (1, 6, 11), (6, 11, 16), (11, 16, 21), (2, 7, 12), (7, 12, 17), (12, 17, 22), (3, 8, 13), (8, 13, 18), (13, 18, 23), (4, 9, 14), (9, 14, 19), (14, 19, 24))
-#         for line in end_condition:
-#             if self.board_a[line[0]] == self.board_a[line[1]] \
-#                     and self.board_a[line[1]] == self.board_a[line[2]] \
-#                     and self.board_a[line[0]] != 0:
-#                 # 종료됐다면 누가 이겼는지 표시
-#                 self.done = True
-#                 self.reward = player
-#                 return
-#         # 비긴 상태는 더는 보드에 빈 공간이 없을때
-#         observation = self.get_action()
-#         if (len(observation)) == 0:
-#             self.done = True
-#             self.reward = 0
-#         return
-#
-#     # 현재 보드의 상태를 표시 p1 = O, p2 = X
-#     def print_board(self):
-#         print("+----+----+----+----+----+")
-#         for i in range(5):
-#             for j in range(5):
-#                 if self.board_a[5 * i + j] == 1:
-#                     print("|  O", end=" ")
-#                 elif self.board_a[5 * i + j] == -1:
-#                     print("|  X", end=" ")
-#                 else:
-#                     print("|   ", end=" ")
-#             print("|")
-#             print("+----+----+----+----+----+")
+    def __init__(self):
+        # 보드는 0으로 초기화된 25개의 배열로 준비
+        # 게임종료 : done = True
+        self.board_a = np.zeros(16)
+        self.done = False
+        self.reward = 0
+        self.winner = 0
+        self.print = False
 
+
+    def move(self, p1, p2, player):
+        # 각 플레이어가 선택한 행동을 표시 하고 게임 상태(진행 또는 종료)를 판단
+        # p1 = 1, p2 = -1로 정의
+        # 각 플레이어는 행동을 선택하는 select_action 메서드를 가짐
+        if player == 1:
+            pos = p1.select_action(env, player)
+        else:
+            pos = p2.select_action(env, player)
+
+        # 보드에 플레이어의 선택을 표시
+        self.board_a[pos] = player
+        if self.print:
+            print(player)
+            self.print_board()
+        # 게임이 종료상태인지 아닌지를 판단
+        self.end_check(player)
+
+        return self.reward, self.done
+
+    # 현재 보드 상태에서 가능한 행동(둘 수 있는 장소)을 탐색하고 리스트로 반환
+    def get_action(self):
+        observation = []
+        for i in range(16):
+            if self.board_a[i] == 0:
+                observation.append(i)
+        return observation
+
+    # 게임이 종료(승패 또는 비김)됐는지 판단
+    def end_check(self, player):
+        # 0 1 2
+        # 3 4 5
+        # 6 7 8
+        # 승패 조건은 가로, 세로, 대각선 이 -1 이나 1 로 동일할 때
+        end_condition = ((0, 1, 2, 3), (4, 5, 6, 7), (8, 9, 10, 11), (12, 13, 14, 15),
+                         (0, 4, 8, 12), (1, 5, 9, 13), (2, 6, 10, 14), (3, 7, 11, 15),
+                         (0, 5, 10, 15), (3, 6, 9, 12))
+        for line in end_condition:
+            if self.board_a[line[0]] == self.board_a[line[1]] \
+                    and self.board_a[line[1]] == self.board_a[line[2]] \
+                    and self.board_a[line[2]] == self.board_a[line[3]] \
+                    and self.board_a[line[0]] != 0:
+                # 종료됐다면 누가 이겼는지 표시
+                self.done = True
+                self.reward = player
+                return
+        # 비긴 상태는 더는 보드에 빈 공간이 없을때
+        observation = self.get_action()
+        if (len(observation)) == 0:
+            self.done = True
+            self.reward = 0
+        return
+
+    # 현재 보드의 상태를 표시 p1 = O, p2 = X
+    def print_board(self):
+        print("+----+----+----+----+")
+        for i in range(4):
+            for j in range(4):
+                if self.board_a[4 * i + j] == 1:
+                    print("|  O", end=" ")
+                elif self.board_a[4 * i + j] == -1:
+                    print("|  X", end=" ")
+                else:
+                    print("|   ", end=" ")
+            print("|")
+            print("+----+----+----+----+")
+
+class Environment55to5():  # 5x5환경 승리조건 5개
+
+    def __init__(self):
+        # 보드는 0으로 초기화된 25개의 배열로 준비
+        # 게임종료 : done = True
+        self.board_a = np.zeros(25)
+        self.done = False
+        self.reward = 0
+        self.winner = 0
+        self.print = False
+
+
+    def move(self, p1, p2, player):
+        # 각 플레이어가 선택한 행동을 표시 하고 게임 상태(진행 또는 종료)를 판단
+        # p1 = 1, p2 = -1로 정의
+        # 각 플레이어는 행동을 선택하는 select_action 메서드를 가짐
+        if player == 1:
+            pos = p1.select_action(env, player)
+        else:
+            pos = p2.select_action(env, player)
+
+        # 보드에 플레이어의 선택을 표시
+        self.board_a[pos] = player
+        if self.print:
+            print(player)
+            self.print_board()
+        # 게임이 종료상태인지 아닌지를 판단
+        self.end_check(player)
+
+        return self.reward, self.done
+
+    # 현재 보드 상태에서 가능한 행동(둘 수 있는 장소)을 탐색하고 리스트로 반환
+    def get_action(self):
+        observation = []
+        for i in range(25):
+            if self.board_a[i] == 0:
+                observation.append(i)
+        return observation
+
+    # 게임이 종료(승패 또는 비김)됐는지 판단
+    def end_check(self, player):
+        # 0 1 2
+        # 3 4 5
+        # 6 7 8
+        # 승패 조건은 가로, 세로, 대각선 이 -1 이나 1 로 동일할 때
+        end_condition = ((0, 1, 2, 3, 4), (5, 6, 7, 8, 9), (10, 11, 12, 13, 14), (15, 16, 17, 18, 19), (20, 21, 22, 23, 24),
+                         (0, 5, 10, 15, 20), (1, 6, 11, 16, 21), (2, 7, 12, 17, 22), (3, 8, 13, 18, 23), (4, 9, 14, 19, 24),
+                         (0, 6, 12, 18, 24), (4, 8, 12, 16, 20))
+        for line in end_condition:
+            if self.board_a[line[0]] == self.board_a[line[1]] \
+                    and self.board_a[line[1]] == self.board_a[line[2]] \
+                    and self.board_a[line[2]] == self.board_a[line[3]] \
+                    and self.board_a[line[3]] == self.board_a[line[4]] \
+                    and self.board_a[line[0]] != 0:
+                # 종료됐다면 누가 이겼는지 표시
+                self.done = True
+                self.reward = player
+                return
+        # 비긴 상태는 더는 보드에 빈 공간이 없을때
+        observation = self.get_action()
+        if (len(observation)) == 0:
+            self.done = True
+            self.reward = 0
+        return
+
+    # 현재 보드의 상태를 표시 p1 = O, p2 = X
+    def print_board(self):
+        print("+----+----+----+----+----+")
+        for i in range(5):
+            for j in range(5):
+                if self.board_a[5 * i + j] == 1:
+                    print("|  O", end=" ")
+                elif self.board_a[5 * i + j] == -1:
+                    print("|  X", end=" ")
+                else:
+                    print("|   ", end=" ")
+            print("|")
+            print("+----+----+----+----+----+")
+
+class Environment55to3():  # 5x5환경 승리조건 3개
+
+    def __init__(self):
+        # 보드는 0으로 초기화된 25개의 배열로 준비
+        # 게임종료 : done = True
+        self.board_a = np.zeros(25)
+        self.done = False
+        self.reward = 0
+        self.winner = 0
+        self.print = False
+
+
+    def move(self, p1, p2, player):
+        # 각 플레이어가 선택한 행동을 표시 하고 게임 상태(진행 또는 종료)를 판단
+        # p1 = 1, p2 = -1로 정의
+        # 각 플레이어는 행동을 선택하는 select_action 메서드를 가짐
+        if player == 1:
+            pos = p1.select_action(env, player)
+        else:
+            pos = p2.select_action(env, player)
+
+        # 보드에 플레이어의 선택을 표시
+        self.board_a[pos] = player
+        if self.print:
+            print(player)
+            self.print_board()
+        # 게임이 종료상태인지 아닌지를 판단
+        self.end_check(player)
+
+        return self.reward, self.done
+
+    # 현재 보드 상태에서 가능한 행동(둘 수 있는 장소)을 탐색하고 리스트로 반환
+    def get_action(self):
+        observation = []
+        for i in range(25):
+            if self.board_a[i] == 0:
+                observation.append(i)
+        return observation
+
+    # 게임이 종료(승패 또는 비김)됐는지 판단
+    def end_check(self, player):
+        # 0 1 2
+        # 3 4 5
+        # 6 7 8
+        # 승패 조건은 가로, 세로, 대각선 이 -1 이나 1 로 동일할 때
+        end_condition = ((0, 1, 2), (1, 2, 3), (2, 3, 4), (5, 6, 7), (6, 7, 8), (7, 8, 9), (10, 11, 12), (11, 12, 13), (12, 13, 14), (15, 16, 17), (16, 17, 18), (17, 18, 19), (20, 21, 22), (21, 22, 23), (22, 23, 24),
+                         (0, 6, 12), (6, 12, 18), (12, 18, 24), (5, 11, 17), (11, 17, 23), (10, 16, 22), (1, 7, 13), (7, 13, 19), (2, 8, 14),
+                         (4, 8, 12), (8, 12, 16), (12, 16, 20), (3, 7, 11), (7, 11, 15), (2, 6, 10), (9, 13, 17), (13, 17, 21), (14, 18, 22),
+                         (0, 5, 10), (5, 10, 15), (10, 15, 20), (1, 6, 11), (6, 11, 16), (11, 16, 21), (2, 7, 12), (7, 12, 17), (12, 17, 22), (3, 8, 13), (8, 13, 18), (13, 18, 23), (4, 9, 14), (9, 14, 19), (14, 19, 24))
+        for line in end_condition:
+            if self.board_a[line[0]] == self.board_a[line[1]] \
+                    and self.board_a[line[1]] == self.board_a[line[2]] \
+                    and self.board_a[line[0]] != 0:
+                # 종료됐다면 누가 이겼는지 표시
+                self.done = True
+                self.reward = player
+                return
+        # 비긴 상태는 더는 보드에 빈 공간이 없을때
+        observation = self.get_action()
+        if (len(observation)) == 0:
+            self.done = True
+            self.reward = 0
+        return
+
+    # 현재 보드의 상태를 표시 p1 = O, p2 = X
+    def print_board(self):
+        print("+----+----+----+----+----+")
+        for i in range(5):
+            for j in range(5):
+                if self.board_a[5 * i + j] == 1:
+                    print("|  O", end=" ")
+                elif self.board_a[5 * i + j] == -1:
+                    print("|  X", end=" ")
+                else:
+                    print("|   ", end=" ")
+            print("|")
+            print("+----+----+----+----+----+")
 
 class Random_player():
 
@@ -252,7 +407,7 @@ class MG_player():
         return action
 
     def Itr_Policy(self, v_table):
-        env = Environment()
+        env = Environment44to4() # 게임 환경 지정
         gamma = 0.9
         k = 0
 
@@ -377,7 +532,7 @@ class KMGPlayer():
 
 np.random.seed(0)
 
-p1 = MG_player()
+# p1 = KMG_player()
 # p2 = MG_player()
 
 # p1 = Human_player()
@@ -386,7 +541,7 @@ p1 = MG_player()
 # p1 = Random_player()
 p2 = Random_player()
 
-# p1 = KMGPlayer()
+p1 = KMGPlayer()
 # p1.num_playout = 100
 # p2 = Monte_Carlo_player()
 # p2.num_playout = 1000
@@ -422,7 +577,7 @@ if auto:
     for j in tqdm(range(games)):
 
         np.random.seed(j)
-        env = Environment()
+        env = Environment55to5()  # 게임 환경 지정
 
         for i in range(10000):
             # p1 과 p2가 번갈아 가면서 게임을 진행
@@ -443,7 +598,7 @@ else:
     np.random.seed(1)
     while True:
 
-        env = Environment()
+        env = Environment55to5() # 게임 환경 지정
         env.print = False
         for i in range(10000):
             reward, done = env.move(p1, p2, (-1) ** i)
@@ -471,6 +626,3 @@ else:
             break
 
 print("p1({}) = {} p2({}) = {} draw = {}".format(p1.name, p1_score, p2.name, p2_score, draw_score))
-
-
-
